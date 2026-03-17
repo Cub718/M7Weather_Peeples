733ddec29c91c7e01d3554e2ba74e7532c426a2c
Brayden Peeples <braydenpeeples@gmail.com>
2026-03-17 12:17:26 -0400

    Updated using function with return statement.

--- M3Weather_Peeples/ViewController.swift
+++ M3Weather_Peeples/ViewController.swift
@@ -16,77 +16,59 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
+        
+        func getWeatherImage(matching weather:String) -> UIImage? {
+            switch weather {
+                case "Sunny":
+                    return UIImage(named: "Sunny")
+                    
+                case "Cloudy":
+                    return UIImage(named: "Cloudy")
+                    
+                case "Rainy":
+                    return UIImage(named: "Rainy")
+                    
+                case "Snowy":
+                    return UIImage(named: "Snowy")
+                    
+                default:
+                    return UIImage(named: "Unknown")
+                    
+            }
+            
+        }
+        
+        func getWeatherDescription(matching temperature: Double) -> String? {
+            switch temperature {
+            case ..<20:
+                return "Extreme cold!"
+            case 20..<50:
+                return "It's cold outside!"
+            case 50..<69:
+                return "Cool and mild!"
+            case 69..<86:
+                return "Warm!"
+            case 86...:
+                return "Heat alert!"
+            default:
+                return "Temperature unknown"
+                
+            }
+        }
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
-        let weather = "Snowy"
+        // Choose an element using nil-coalescing
+        let weather = ["Sunny", "Cloudy", "Rainy", "Snowy"].randomElement() ?? "Unknown"
         let temperature = 19.5
         
-        /*
-        if weather == "Sunny" {
-            weatherIcon.image = UIImage(named: "Sunny")
-            weatherText.text = "Sunny"
-            self.temperature.text = "\(temperature)°F"
-        }
-        else if weather == "Cloudy" {
-            weatherIcon.image = UIImage(named: "Cloudy")
-            weatherText.text = "Cloudy"
-            self.temperature.text = "\(temperature)°F"
-        }
-        else if weather == "Rainy" {
-            weatherIcon.image = UIImage(named: "Rainy")
-            weatherText.text = "Rainy"
-            self.temperature.text = "\(temperature)°F"
-        }
-        else if weather == "Snowy" {
-            weatherIcon.image = UIImage(named: "Snowy")
-            weatherText.text = "Snowy"
-            self.temperature.text = "\(temperature)°F"
-        }
-        else {
-            weatherIcon.image = UIImage(named: "Unknown")
-            weatherText.text = "Unknown"
-            self.temperature.text = "\(temperature)°F"
-        }
-        */
-        
-        switch weather {
-            case "Sunny":
-                weatherIcon.image = UIImage(named: "Sunny")
-                weatherText.text = "Sunny"
-            case "Cloudy":
-                weatherIcon.image = UIImage(named: "Cloudy")
-                weatherText.text = "Cloudy"
-            case "Rainy":
-                weatherIcon.image = UIImage(named: "Rainy")
-                weatherText.text = "Rainy"
-            case "Snowy":
-                weatherIcon.image = UIImage(named: "Snowy")
-                weatherText.text = "Snowy"
-            default:
-                weatherIcon.image = UIImage(named: "Unknown")
-                weatherText.text = "Unknown"
-                break
-        }
-        
+        weatherIcon.image = getWeatherImage(matching: weather)
+        weatherText.text = weather
+        tempWarning.text = getWeatherDescription(matching: temperature)
         self.temperature.text = "\(temperature)°F"
         
-        switch temperature {
-        case ..<20:
-            tempWarning.text = "Extreme cold!"
-        case 20..<50:
-            tempWarning.text = "It's cold outside!"
-        case 50..<69:
-            tempWarning.text = "Cool and mild!"
-        case 69..<86:
-            tempWarning.text = "Warm!"
-        case 86...:
-            tempWarning.text = "Heat alert!"
-        default:
-            tempWarning.text = "Temperature unknown"
-            break
-        }
+        
         
     }
 
